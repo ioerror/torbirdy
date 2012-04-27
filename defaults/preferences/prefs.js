4b0ee184a8f6b2ecd0e88a79f1f77d3cca993bb1
Sukhbir Singh <sukhbir.in@gmail.com>
2012-04-27 22:29:06 +0530

    Fixed the installation issue and implemented basic support for Torbutton

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,9 +1,25 @@
 /*
-  Default pref values for Torbutton for Thunderbird
+  Default preference values for Torbutton for Thunderbird.
 */
 
-pref("network.proxy.socks_remote_dns", "True");
+// Network settings.
+pref("network.proxy.socks_remote_dns", true);
 pref("network.proxy.socks", "127.0.0.1");
-pref("network.proxy.socks_port","9050");
-pref("network.proxy.socks_version","5");
-pref("network.proxy.no_proxies_on","localhost, 127.0.0.1");
+pref("network.proxy.socks_port", 9050);
+pref("network.proxy.socks_version", 5);
+pref("network.proxy.no_proxies_on", "localhost, 127.0.0.1");
+pref("network.proxy.type", 1);
+
+// Override the user agent (empty string).
+pref("general.useragent.override", "");
+
+// Disable automatic updates.
+pref("app.update.enabled", false);
+
+// Prevent hostname leaks.
+pref("mail.smtpserver.default.hello_argument", "localhost");
+
+// Disable HTML email composing.
+pref("mail.html_compose", false);
+pref("mail.identity.default.compose_html", false);
+pref("mail.default_html_action", 1)
