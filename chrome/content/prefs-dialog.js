7babd4a99ec54cb838f588295b82e50c958e5795
Jacob Appelbaum <jacob@appelbaum.net>
2012-07-06 19:30:20 +0200

    whitespace fixup

--- chrome/content/prefs-dialog.js
+++ chrome/content/prefs-dialog.js
@@ -23,25 +23,24 @@ function onAccept() {
       .getService(Ci.nsIPrefBranch);
   var win = Cc['@mozilla.org/appshell/window-mediator;1']
       .getService(Ci.nsIWindowMediator)
-      .getMostRecentWindow('mail:3pane'); 
+      .getMostRecentWindow('mail:3pane');
   var myPanel = win.document.getElementById("my-panel");
-  var AnonService = document.getElementById('AnonService').selectedIndex; 
+  var AnonService = document.getElementById('AnonService').selectedIndex;
 
   myprefsHandler.setIntPref('extensions.torbirdy.proxy', AnonService);
   
   if (AnonService == 0) {
-      // Set proxies for Tor 
+      // Set proxies for Tor
       myprefsHandler.setIntPref("network.proxy.socks_port", 9050);
       myprefsHandler.setIntPref("network.proxy.ssl_port", 8118);
       myprefsHandler.setIntPref("network.proxy.http_port", 8118);
       myPanel.label = "Tor Enabled";
   } else {
-      // Set proxies for JonDo 
+      // Set proxies for JonDo
       myprefsHandler.setIntPref("network.proxy.socks_port", 4001);
       myprefsHandler.setIntPref("network.proxy.ssl_port", 4001);
       myprefsHandler.setIntPref("network.proxy.http_port", 4001);
       myPanel.label = "JonDo Enabled";
   }
-      
 }
 
