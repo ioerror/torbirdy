0b3ef455011270875730fedb77fb1ad2b0339efa
Sukhbir Singh <sukhbir.in@gmail.com>
2012-08-01 13:47:28 -0400

    Updated the preferences dialog to adjust check new mail setting

--- chrome/content/accountpref.js
+++ chrome/content/accountpref.js
@@ -4,6 +4,10 @@ var account = window.arguments[0];
 function onLoad() {
   var nameLabel = document.getElementById("account-name");
   var startup = document.getElementById("check-startup");
+
+  var dobiff = document.getElementById("check-new");
+  var biffminutes = document.getElementById("check-minutes");
+
   nameLabel.value = account.prettyName;
 
   if (account.loginAtStartUp) {
@@ -11,13 +15,41 @@ function onLoad() {
   } else {
     startup.checked = false;
   }
+
+  if (account.doBiff) {
+    dobiff.checked = true;
+    biffminutes.disabled = false;
+    biffminutes.value = account.biffMinutes;
+  } else {
+    dobiff.checked = false;
+    biffminutes.disabled = true;
+  }
 }
 
 function onAccept() {
-  var startup = document.getElementById('check-startup');
+  var startup = document.getElementById("check-startup");
+  var dobiff = document.getElementById("check-new");
+  var biffminutes = document.getElementById("check-minutes");
+
   if (startup.checked) {
     account.loginAtStartUp = true;
   } else {
     account.loginAtStartUp = false;
+  }
+
+  if (dobiff.checked) {
+    account.doBiff = true;
+    account.biffMinutes = biffminutes.value;
+  } else {
+    account.doBiff = false;
+  }
+}
+
+function onToggleCheck() {
+  var biffminutes = document.getElementById("check-minutes");
+  if (biffminutes.disabled) {
+    biffminutes.disabled = false;
+  } else {
+    biffminutes.disabled = true;
   }
 }
