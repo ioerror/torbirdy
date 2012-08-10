6a359323b5a4b3a4630f65bfb62e1ed531ac01c7
Sukhbir Singh <sukhbir.in@gmail.com>
2012-08-10 17:36:11 -0400

    Update the preferences to reflect the folder select state

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -2,3 +2,4 @@ pref("extensions.torbirdy.protected", false);
 pref("extensions.torbirdy.proxy", 0);
 pref("extensions.torbirdy.first_run", true);
 pref("extensions.torbirdy.warn", true);
+pref("extensions.torbirdy.startup_folder", false);
