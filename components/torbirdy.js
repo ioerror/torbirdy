7dffbcd38cd22e07d74ecebb0d7baccd3a9217f3
Sukhbir Singh <sukhbir.in@gmail.com>
2013-01-29 22:37:11 -0500

    Clear the HTTP and SSL proxies (if set) when TorBirdy runs

--- components/torbirdy.js
+++ components/torbirdy.js
@@ -46,6 +46,13 @@ const TORBIRDYPREFS = {
   "network.proxy.socks_version": 5,
   "network.proxy.no_proxies_on": "localhost, 127.0.0.1",
 
+  // Clear the HTTP and SSL proxies.
+  // We restore these settings when TorBirdy is uninstalled.
+  "network.proxy.http": "",
+  "network.proxy.http_port": 0,
+  "network.proxy.ssl": "",
+  "network.proxy.ssl_port": 0,
+
   // Set DNS proxying through SOCKS5.
   "network.proxy.socks_remote_dns": true,
 
