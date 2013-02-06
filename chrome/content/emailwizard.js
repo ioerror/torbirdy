8ee68d9c40ab081914510e4251e6c0abe11434d1
Sukhbir Singh <sukhbir.in@gmail.com>
2013-02-06 14:57:28 -0500

    Added support for Tails
    If TorBirdy is running on Tails, allow the automatic configuration
    wizard (and disable our manual configuration overlay) since Tails
    handles that on its own. For more information, see:
    https://tails.boum.org/todo/Return_of_Icedove__63__/#index6h2

--- chrome/content/emailwizard.js
+++ chrome/content/emailwizard.js
@@ -4,94 +4,122 @@ if (!org.torbirdy) org.torbirdy = {};
 org.torbirdy.emailwizard = new function() {
   var pub = {};
 
+  var prefs = Cc["@mozilla.org/preferences-service;1"]
+                .getService(Ci.nsIPrefBranch);
+
+  // Check if we are running Tails. If yes, disable the manual account
+  // configuration wizard since Tails handles that on its own. See:
+  // https://tails.boum.org/todo/Return_of_Icedove__63__/#index6h2
+  var runningTails = false;
+  if (prefs.prefHasUserValue("vendor.name")) {
+    if (prefs.getCharPref("vendor.name") === "Tails") {
+      runningTails = true;
+    }
+  }
+
   pub.disableAutoWizard = function() {
-    var realname = document.getElementById("realname").value;
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    var remember_password = document.getElementById("remember_password").checked;
-    var protocol = document.getElementById("torbirdy-protocol").value;
+    if (!runningTails) {
+      var realname = document.getElementById("realname").value;
+      var email = document.getElementById("email").value;
+      var password = document.getElementById("password").value;
+      var remember_password = document.getElementById("remember_password").checked;
+      var protocol = document.getElementById("torbirdy-protocol").value;
 
-    var prompts = Cc["@mozilla.org/embedcomp/prompt-service;1"]
-                                  .getService(Ci.nsIPromptService);
+      var prompts = Cc["@mozilla.org/embedcomp/prompt-service;1"]
+                                    .getService(Ci.nsIPromptService);
 
-    var bundles = Cc["@mozilla.org/intl/stringbundle;1"]
-                            .getService(Ci.nsIStringBundleService);
-    var strings = bundles.createBundle("chrome://castironthunderbirdclub/locale/torbirdy.properties");
-    var emailPrompt = strings.formatStringFromName("torbirdy.email.prompt", [email], 1);
-    var extName = strings.GetStringFromName("torbirdy.name");
-    prompts.alert(null, extName, emailPrompt);
+      var bundles = Cc["@mozilla.org/intl/stringbundle;1"]
+                              .getService(Ci.nsIStringBundleService);
+      var strings = bundles.createBundle("chrome://castironthunderbirdclub/locale/torbirdy.properties");
+      var emailPrompt = strings.formatStringFromName("torbirdy.email.prompt", [email], 1);
+      var extName = strings.GetStringFromName("torbirdy.name");
+      prompts.alert(null, extName, emailPrompt);
 
-    var config = new AccountConfig();
-    config.incoming.type = protocol;
+      var config = new AccountConfig();
+      config.incoming.type = protocol;
 
-    config.incoming.username = "%EMAILLOCALPART%";
-    config.outgoing.username = "%EMAILLOCALPART%";
+      config.incoming.username = "%EMAILLOCALPART%";
+      config.outgoing.username = "%EMAILLOCALPART%";
 
-    if (protocol === "imap") {
-      config.incoming.hostname = "imap.%EMAILDOMAIN%";
-      config.incoming.port = 993;
+      if (protocol === "imap") {
+        config.incoming.hostname = "imap.%EMAILDOMAIN%";
+        config.incoming.port = 993;
+      }
+
+      if (protocol === "pop3") {
+        config.incoming.hostname = "pop.%EMAILDOMAIN%";
+        config.incoming.port = 995;
+      }
+
+      // Default to SSL for both outgoing and incoming servers.
+      config.incoming.socketType = 2;
+      config.outgoing.socketType = 2;
+
+      // Set the authentication to 'Normal' (connection is already encrypted).
+      config.incoming.auth = 3;
+      config.outgoing.auth = 3;
+
+      // Set default values to disable automatic email fetching.
+      config.incoming.loginAtStartup = false;
+      config.incoming.downloadOnBiff = false;
+
+      // Default the outgoing SMTP port.
+      config.outgoing.port = 465;
+
+      config.outgoing.hostname = "smtp.%EMAILDOMAIN%";
+
+      replaceVariables(config, realname, email, password);
+      config.rememberPassword = remember_password && !!password;
+
+      var newAccount = createAccountInBackend(config);
+
+      // Set check_new_mail to false. We can't do this through the account setup, so let's do it here.
+      const checkNewMail = 'mail.server.%serverkey%.check_new_mail';
+      const serverkey = newAccount.incomingServer.key;
+      var checkNewMailPref = checkNewMail.replace("%serverkey%", serverkey);
+      prefs.setBoolPref(checkNewMailPref, false);
+
+      // From comm-release/mailnews/base/prefs/content/accountcreation/emailWizard.js : onAdvancedSetup().
+      var windowManager = Cc["@mozilla.org/appshell/window-mediator;1"]
+          .getService(Ci.nsIWindowMediator);
+      var existingAccountManager = windowManager
+          .getMostRecentWindow("mailnews:accountmanager");
+      if (existingAccountManager) {
+        existingAccountManager.focus();
+      } else {
+        window.openDialog("chrome://messenger/content/AccountManager.xul",
+                          "AccountManager", "chrome,centerscreen,modal,titlebar",
+                          { server: newAccount.incomingServer,
+                            selectPage: "am-server.xul" });
+      }
+      window.close();
     }
-
-    if (protocol === "pop3") {
-      config.incoming.hostname = "pop.%EMAILDOMAIN%";
-      config.incoming.port = 995;
+    else {
+      gEmailConfigWizard.onNext();
     }
-
-    // Default to SSL for both outgoing and incoming servers.
-    config.incoming.socketType = 2;
-    config.outgoing.socketType = 2;
-
-    // Set the authentication to 'Normal' (connection is already encrypted).
-    config.incoming.auth = 3;
-    config.outgoing.auth = 3;
-
-    // Set default values to disable automatic email fetching.
-    config.incoming.loginAtStartup = false;
-    config.incoming.downloadOnBiff = false;
-
-    // Default the outgoing SMTP port.
-    config.outgoing.port = 465;
-
-    config.outgoing.hostname = "smtp.%EMAILDOMAIN%";
-
-    replaceVariables(config, realname, email, password);
-    config.rememberPassword = remember_password && !!password;
-
-    var newAccount = createAccountInBackend(config);
-
-    // Set check_new_mail to false. We can't do this through the account setup, so let's do it here.
-    const checkNewMail = 'mail.server.%serverkey%.check_new_mail';
-    const serverkey = newAccount.incomingServer.key;
-    var prefs = Cc["@mozilla.org/preferences-service;1"]
-                  .getService(Ci.nsIPrefBranch);
-    var checkNewMailPref = checkNewMail.replace("%serverkey%", serverkey);
-    prefs.setBoolPref(checkNewMailPref, false);
-
-    // From comm-release/mailnews/base/prefs/content/accountcreation/emailWizard.js : onAdvancedSetup().
-    var windowManager = Cc["@mozilla.org/appshell/window-mediator;1"]
-        .getService(Ci.nsIWindowMediator);
-    var existingAccountManager = windowManager
-        .getMostRecentWindow("mailnews:accountmanager");
-    if (existingAccountManager) {
-      existingAccountManager.focus();
-    } else {
-      window.openDialog("chrome://messenger/content/AccountManager.xul",
-                        "AccountManager", "chrome,centerscreen,modal,titlebar",
-                        { server: newAccount.incomingServer,
-                          selectPage: "am-server.xul" });
-    }
-    window.close();
   };
 
   pub.onKeyEnter = function(event) {
     var keycode = event.keyCode;
     if (keycode == 13) {
-      pub.disableAutoWizard();
+      if (document.getElementById("next_button").disabled === false) {
+        if (!runningTails) {
+          pub.disableAutoWizard();
+        }
+        else {
+          gEmailConfigWizard.onNext();
+        }
+      }
     }
   };
 
   pub.onLoad = function(event) {
-    document.getElementById('provisioner_button').setAttribute('disabled', 'true');
+    if (!runningTails) {
+      document.getElementById("provisioner_button").disabled = true;
+    }
+    else {
+      document.getElementById("torbirdy-protocol-box").collapsed = true;
+    }
   };
 
   return pub;
