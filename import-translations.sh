10dc44d9a622d4069d7e16665de9c79113c2d854
Sukhbir Singh <sukhbir@torproject.org>
2013-11-22 14:03:12 -0500

    Add script to import translations

---
+++ import-translations.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+BUNDLE_LOCALES="ar da eu he lv pa pt_BR tr cs de es fr it nl pl sv zh-CN ms-MY ja"
+
+if [ -d translation ];
+then
+  cd translation
+  git fetch origin
+  cd ..
+else
+  git clone https://git.torproject.org/translation.git
+fi
+
+cd translation
+for i in $BUNDLE_LOCALES
+do
+  UL="`echo $i|tr - _`"
+
+  git checkout torbirdy
+  git merge origin/torbirdy
+  cp $UL/torbirdy.dtd ../chrome/locale/$i/torbirdy.dtd
+  cp $UL/torbirdy.properties ../chrome/locale/$i/torbirdy.properties
+
+done
